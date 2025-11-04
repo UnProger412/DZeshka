d1ae2659d2d456ce69be0545a187cc43bb5c307a
UnProger412 <tikhon.baskin@yandex.ru>
2025-11-04 15:02:17 +0300

    DZ fix

--- code/index.php
+++ code/index.php
@@ -1,7 +1,34 @@
 <?php
 
+include __DIR__ . '/vendor/autoload.php';
+
+use \Predis\Client;
+
 echo "Dodix Привет, Otus!<br>".date("Y-m-d H:i:s") ."<br><br>";
 
-echo "Что-то еще";
+echo "Что-то еще<br>";
+$client = new Client(
+    ['host' => 'redis', 'port' => 6379]
+);
 
-phpinfo();
+echo extension_loaded('memcached') ? 'да' : 'пизда';
+echo '<br>';
+
+$mc = new Memcached();
+
+$mc->addServer("memcached", 11211);
+
+$mc->set("foo", "Hello!");
+
+echo $mc->get("foo") . "<br>";
+
+echo $mc->getVersion();
+
+print_r($mc->getVersion());
+
+echo count($mc->getServerList()) . "<br>";
+
+$client->set('foo', 'bar');
+$value = $client->get('foo');
+
+echo $value;
